5474d78e5b1a8bbe07449935f68b35caa4b9b0ce
eyaskin23 <evelynyaskin@gmail.com>
2023-10-31 19:19:54 -0400

    Adding testing and comment

--- test/exampleHexShape.java
+++ test/exampleHexShape.java
@@ -24,5 +24,37 @@ public class exampleHexShape {
   public void testS() {
     HexShape hex = new HexShape(7, 7, PlayerType.EMPTY);
     Assert.assertEquals(hex.getSValue(), "-14");
+    HexShape hexExample = new HexShape(-1, 0, PlayerType.EMPTY);
+    Assert.assertEquals(hexExample.getSValue(), "1");
+  }
+
+  @Test
+  public void testGetPlayerType() {
+    HexShape hex = new HexShape(7, 7, PlayerType.EMPTY);
+    Assert.assertEquals(hex.getPlayerType(), PlayerType.EMPTY);
+    HexShape stuff = new HexShape(7, 7, null);
+    Assert.assertEquals(stuff.getPlayerType(), PlayerType.EMPTY);
+    HexShape stuffWithBlack = new HexShape(7, 7, PlayerType.BLACK);
+    Assert.assertEquals(stuffWithBlack.getPlayerType(), PlayerType.BLACK);
+  }
+
+
+  @Test
+  public void testSetPlayerType() {
+    HexShape hex = new HexShape(7, 7, PlayerType.EMPTY);
+    Assert.assertEquals(hex.getPlayerType(), PlayerType.EMPTY);
+    HexShape stuff = new HexShape(7, 7, null);
+    Assert.assertEquals(stuff.getPlayerType(), PlayerType.EMPTY);
+    HexShape stuffWithBlack = new HexShape(7, 7, PlayerType.BLACK);
+    Assert.assertEquals(stuffWithBlack.getPlayerType(), PlayerType.BLACK);
+
+    hex.setPlayerType(PlayerType.BLACK);
+    Assert.assertEquals(hex.getPlayerType(), PlayerType.BLACK);
+
+    stuff.setPlayerType(PlayerType.BLACK);
+    Assert.assertEquals(stuff.getPlayerType(), PlayerType.BLACK);
+
+    stuffWithBlack.setPlayerType(PlayerType.WHITE);
+    Assert.assertEquals(stuffWithBlack.getPlayerType(), PlayerType.WHITE);
   }
 }

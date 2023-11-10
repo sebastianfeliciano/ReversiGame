f83d4f5ac137beac51cc0baaf95e29359f05f636
Sebastian Feliciano <feliciano.sebastian2004@gmail.com>
2023-11-10 14:52:44 -0500

    Allowing the recognition of which HexShape what chosen from a click.

--- src/view/HexagonDrawn.java
+++ src/view/HexagonDrawn.java
@@ -1,17 +1,5 @@
 package view;
 
-import model.HexShape;
-
 public class HexagonDrawn {
-  HexShape hexShape;
-  public HexagonDrawn(HexShape hexShape){
-    this.hexShape = hexShape;
-  }
-
-
-
-
-
-
 
 }

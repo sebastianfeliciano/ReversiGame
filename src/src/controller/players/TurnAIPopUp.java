a5a1fb8219145807ec6f85c9662b8b5cc44c1ee8
Sebastian Feliciano <115121233+sebastianf-8114@users.noreply.github.com>
2023-12-10 17:08:16 -0500

    Delete src directory

--- src/src/controller/players/TurnAIPopUp.java
+++
@@ -1,9 +0,0 @@
-package controller.players;
-
-
-/**
- * An interface that tells the AI it is their turn and should ignore the pop-up message.
- */
-public interface TurnAIPopUp {
-  void itIsNowYourTurnMessage();
-}
